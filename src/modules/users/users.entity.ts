393f0deef4136466bed5b4ef1e60916d4ef6b96b
EsraaFarhat <esraafarhat97@gmail.com>
2025-06-30 21:30:18 +0300

    Add User Entitiy, Auth Module, and Users Module

---
+++ src/modules/users/users.entity.ts
@@ -0,0 +1,41 @@
+import { UserRole } from 'src/common/enums/role.enum';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ nullable: true })
+  avatarUrl: string;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.USER })
+  role: UserRole;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column({ nullable: true })
+  lastLoginAt: Date;
+}
